3bb280ebab88319dc9b57e2149f84df5b4fd920f
Sirishakopparaju <sirishakopparaju98@gmail.com>
2022-05-06 18:58:47 +0530

    Added:indian state code

---
+++ lib/src/test/java/DesignPrinciples/CensusAnalyzerStateCodeTest.java
@@ -0,0 +1,21 @@
+package DesignPrinciples;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CensusAnalyzerStateCodeTest {
+
+    private static final String INDIA_CENSUS_CSV_PATH = "D:\\Design\\DesignPrinciples\\lib\\src\\test\\resources\\IndiaStateCode.CSV";
+
+    @Test
+    public void givenIndianCensusCSVFileReturnsCorrectRecords() {
+        CensusAnalyser censusAnalyzer = new CensusAnalyser();
+        try {
+            int numOfRecord = censusAnalyzer.loadIndiaCensusData(INDIA_CENSUS_CSV_PATH);
+            Assert.assertEquals(37, numOfRecord);
+        } catch (CensusAnalyserException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
