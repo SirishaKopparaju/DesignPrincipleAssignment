454fd15287cd26e12fa8ca0c17fc97771ded111e
Sirishakopparaju <sirishakopparaju98@gmail.com>
2022-05-06 19:16:11 +0530

    Refactor:file type is incorrect

--- lib/src/test/java/DesignPrinciples/CensusAnalyzerStateCodeTest.java
+++ lib/src/test/java/DesignPrinciples/CensusAnalyzerStateCodeTest.java
@@ -5,13 +5,14 @@ import org.junit.Test;
 
 public class CensusAnalyzerStateCodeTest {
 
-    private static final String INDIA_CENSUS_CSV_PATH = "D:\\Design\\DesignPrinciples\\lib\\src\\test\\resources\\IndiaStateCode.CSV";
+	private static final String INDIA_STATE_CSV_PATH = "D:\\Design\\DesignPrinciples\\lib\\src\\test\\resources\\IndiaStateCode.csv";
+    private static final String INDIA_STATE_CSV_WRONG_FILETYPE = "E:\\\\eclipseProgram\\\\untitled\\\\Census-Analyzer\\src\\main\\resources\\IndiaStateCode.pdf";
 
     @Test
     public void givenIndianCensusCSVFileReturnsCorrectRecords() {
         CensusAnalyser censusAnalyzer = new CensusAnalyser();
         try {
-            int numOfRecord = censusAnalyzer.loadIndiaCensusData(INDIA_CENSUS_CSV_PATH);
+            int numOfRecord = censusAnalyzer.loadIndiaCensusData(INDIA_STATE_CSV_WRONG_FILETYPE);
             Assert.assertEquals(37, numOfRecord);
         } catch (CensusAnalyserException e) {
             e.printStackTrace();
@@ -21,8 +22,18 @@ public class CensusAnalyzerStateCodeTest {
     public void givenIndianStateCSVFileReturnsInCorrectRecords() {
         CensusAnalyser censusAnalyzer = new CensusAnalyser();
         try {
-            int numOfRecord = censusAnalyzer.loadIndiaStateCodeData(INDIA_STATE_CSV_PATH);
+            int numOfRecord = censusAnalyzer.loadIndiaCensusData(INDIA_STATE_CSV_PATH);
             Assert.assertEquals(40, numOfRecord);
+        } catch (CensusAnalyserException e) {
+            e.printStackTrace();
+        }
+    }
+    @Test
+    public void givenIndianStateCSVFileReturnsInCorrecFileType_But_PathShouldBeCorrect() {
+        CensusAnalyser censusAnalyzer = new CensusAnalyser();
+        try {
+            int numOfRecord = censusAnalyzer.loadIndiaCensusData(INDIA_STATE_CSV_WRONG_FILETYPE);
+            Assert.assertEquals(29, numOfRecord);
         } catch (CensusAnalyserException e) {
             e.printStackTrace();
         }
